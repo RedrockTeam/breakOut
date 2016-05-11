77c978bb3fadf5826276ed1d8d2ff0350e0781b4
Hangeer <591417089@qq.com>
2016-05-11 18:12:18 +0800

    :blush:Go to have dinner:blush:

--- src/js/game.js
+++ src/js/game.js
@@ -10,6 +10,7 @@ $(document).ready(() => {
     }
     /* 如果屏幕比预设值高, 那么更改 canvas 的高度 */
 
+    const winHeight = window.innerHeight;
     const $pause = $("#pause");
     $pause.on('touchstart', () => {
         /*
@@ -82,6 +83,22 @@ $(document).ready(() => {
         currentLevel: 0,
         rolled: 0,
         renderBarrier: [0, 8],
+        barrierHeightArr: [
+            winHeight - 400 - 10,
+            winHeight - 670 - 10,
+            winHeight - 1000 - 10,
+            winHeight - 1400 - 10,
+            winHeight - 1700 - 10,
+            winHeight - 2150 - 10,
+            winHeight - 2350 - 10,
+            winHeight - 2650 - 10
+        ],
+        judgeRenderArr: [
+            [120, 410]
+        ],
+        renderArr: [
+
+        ],
         judgeRender () {
             //if (this.rolled >= 120 && this.rolled < 410) {
             //    this.renderBarrier = [1, 2];
@@ -90,22 +107,20 @@ $(document).ready(() => {
             //}
         },
         judgeLevel () {
-            const reach = star.reachHeight;
+            const reach = star.top;
+            let arr = this.barrierHeightArr;
+            let tempLevel = 0;
 
-            if (reach <= 150) {
-                this.currentLevel = 1;
+            arr.forEach((item, index) => {
+                if (reach <= item) {
+                    tempLevel = index + 1;
+                }
+            });
+
+            if (tempLevel > this.currentLevel) {
+                this.currentLevel = tempLevel;
+                $gameBarrier.text(this.currentLevel);
             }
-
-            if (reach <= -170) {
-                this.currentLevel = 2;
-            }
-
-            $gameBarrier.text(this.currentLevel);
-
-            /*
-             *   妈的里面的数据要手动改
-             *   考虑下优雅的写法
-             * */
         },
         stopTimer () {
             window.clearInterval(this.timer);
@@ -141,60 +156,60 @@ $(document).ready(() => {
             'barrier_one_br.move()',
             'barrier_one_tr.move()',
             'barrier_one_tl.move()',
-            'sign_one.paint()',
-            'star.collision(barrier_one_bl.testPoint, barrier_one_bl.isClose, 10)',
-            'star.collision(barrier_one_tl.testPoint, barrier_one_tl.isClose, 10)'
+            'sign_one.paint()'
+            //'star.collision(barrier_one_bl.testPoint, barrier_one_bl.isClose, 10)',
+            //'star.collision(barrier_one_tl.testPoint, barrier_one_tl.isClose, 10)'
         ],
 
         [
             'barrier_two_b.move()',
             'barrier_two_t.move()',
-            'sign_two.paint()',
-            'star.collision(barrier_two_b.testPoint, barrier_two_b.isClose, 20)',
-            'star.collision(barrier_two_t.testPoint, barrier_two_t.isClose, 20)'
+            'sign_two.paint()'
+            //'star.collision(barrier_two_b.testPoint, barrier_two_b.isClose, 20)',
+            //'star.collision(barrier_two_t.testPoint, barrier_two_t.isClose, 20)'
         ],
 
         [
             'barrier_three.rotate()',
-            'sign_three.paint()',
-            'star.collision(barrier_three.testPoint.down.y, barrier_three.testPoint.down.status, 24)',
-            'star.collision(barrier_three.testPoint.up.y, barrier_three.testPoint.up.status, 24)'
+            'sign_three.paint()'
+            //'star.collision(barrier_three.testPoint.down.y, barrier_three.testPoint.down.status, 24)',
+            //'star.collision(barrier_three.testPoint.up.y, barrier_three.testPoint.up.status, 24)'
         ],
 
         [
             'barrier_four.rotate()',
-            'sign_four.paint()',
-            'star.collision(barrier_four.testPoint.down.y, barrier_four.testPoint.down.status, 24)',
-            'star.collision(barrier_four.testPoint.up.y, barrier_four.testPoint.up.status, 24)'
+            'sign_four.paint()'
+            //'star.collision(barrier_four.testPoint.down.y, barrier_four.testPoint.down.status, 24)',
+            //'star.collision(barrier_four.testPoint.up.y, barrier_four.testPoint.up.status, 24)'
         ],
 
         [
             'barrier_five.rotate()',
-            'sign_five.paint()',
-            'star.collision(barrier_five.testPoint.down.y, barrier_five.testPoint.down.status, 24)',
-            'star.collision(barrier_five.testPoint.up.y, barrier_five.testPoint.up.status, 24)'
+            'sign_five.paint()'
+            //'star.collision(barrier_five.testPoint.down.y, barrier_five.testPoint.down.status, 24)',
+            //'star.collision(barrier_five.testPoint.up.y, barrier_five.testPoint.up.status, 24)'
         ],
 
         [
             'barrier_six.rotate()',
-            'sign_six.paint()',
-            'star.collision(barrier_six.testPoint.down.y, barrier_six.testPoint.down.status, 24)',
-            'star.collision(barrier_six.testPoint.up.y, barrier_six.testPoint.up.status, 24)'
+            'sign_six.paint()'
+            //'star.collision(barrier_six.testPoint.down.y, barrier_six.testPoint.down.status, 24)',
+            //'star.collision(barrier_six.testPoint.up.y, barrier_six.testPoint.up.status, 24)'
         ],
 
         [
             'barrier_seven_mountain.move()',
             'barrier_seven_grass.move()',
-            'sign_seven.paint()',
-            'star.collision(barrier_seven_mountain, barrier_seven_mountain.isClose, 18)',
-            'star.collision(barrier_seven_grass.testPoint, barrier_seven_grass.isClose, 18)'
+            'sign_seven.paint()'
+            //'star.collision(barrier_seven_mountain, barrier_seven_mountain.isClose, 18)',
+            //'star.collision(barrier_seven_grass.testPoint, barrier_seven_grass.isClose, 18)'
         ],
 
         [
             'barrier_eight.rotate()',
-            'sign_eight.paint()',
-            'star.collision(barrier_eight.testPoint.down.y, barrier_eight.testPoint.down.status, 24)',
-            'star.collision(barrier_eight.testPoint.up.y, barrier_eight.testPoint.up.status, 24)'
+            'sign_eight.paint()'
+            //'star.collision(barrier_eight.testPoint.down.y, barrier_eight.testPoint.down.status, 24)',
+            //'star.collision(barrier_eight.testPoint.up.y, barrier_eight.testPoint.up.status, 24)'
         ]
 
 
@@ -267,6 +282,8 @@ $(document).ready(() => {
                 pub.judgeLevel();
 
                 //console.log(pub.rolled);
+                //console.log(star.top);
+                //console.log(pub.barrierHeightArr);
             }, 1000/60);
 
         }
@@ -288,7 +305,7 @@ $(document).ready(() => {
 
             this.timer = null;
             this.exp = .1;
-            this.reachHeight = this.top;
+            //this.reachHeight = window.innerHeight - this.top;
         }
 
         paint () {
@@ -404,7 +421,6 @@ $(document).ready(() => {
             this.context.save();
             this.context.translate(this.start.x + .5 * this.width, this.start.y + .5 * this.height);
             this.context.rotate(this.rotateDeg);
-            //this.context.clearRect(-.5 * this.width, -.5 *this.height, this.width, this.height);
             this.context.restore();
             /* 通过改变画布的相对位置来进行重绘 */
 
@@ -589,12 +605,11 @@ $(document).ready(() => {
         return arr[Math.floor(Math.random()*len)];
     }
     /*
-    *   @params:
-    *       randomCircleSpeed: 随机返回圆形的旋转速度
-    *       randomBlockSpeed: 随机返回方块的移动速度
-    * */
+     *   @params:
+     *       randomCircleSpeed: 随机返回圆形的旋转速度
+     *       randomBlockSpeed: 随机返回方块的移动速度
+     * */
 
-    const winHeight = window.innerHeight;
     const stage = new Stage();
     const touch = new Sign({
         left: 167,
@@ -612,12 +627,12 @@ $(document).ready(() => {
 
     });
     /*
-    *   @params:
-    *       winHeight: 屏幕可见区域的高度
-    *       stage: 游戏运行的舞台
-    *       touch: 闪动触摸提醒
-    *       star: 星星
-    * */
+     *   @params:
+     *       winHeight: 屏幕可见区域的高度
+     *       stage: 游戏运行的舞台
+     *       touch: 闪动触摸提醒
+     *       star: 星星
+     * */
     const barrier_one_bl = new Block({
         left: 0,
         top: winHeight - 300,
@@ -674,14 +689,14 @@ $(document).ready(() => {
         img: document.querySelector("#img-title-1")
     });
     /*
-    *   第一关: 冲破四道封锁线
-    *   @params
-    *       barrier_one_bl: 四个小块左下方一块 (bottom left)
-    *       barrier_one_br: 四个小块右下方一块 (bottom right)
-    *       barrier_one_tl: 四个小块左上方一块 (top left)
-    *       barrier_one_tr: 四个小方块右上方一块 (top right)
-    *       sign_one: 关卡标志
-    * */
+     *   第一关: 冲破四道封锁线
+     *   @params
+     *       barrier_one_bl: 四个小块左下方一块 (bottom left)
+     *       barrier_one_br: 四个小块右下方一块 (bottom right)
+     *       barrier_one_tl: 四个小块左上方一块 (top left)
+     *       barrier_one_tr: 四个小方块右上方一块 (top right)
+     *       sign_one: 关卡标志
+     * */
     const barrier_two_b = new Block({
         left: 0,
         top: winHeight - 530,
@@ -714,12 +729,12 @@ $(document).ready(() => {
         img: document.querySelector("#img-title-2")
     });
     /*
-    *   第二关: 强渡乌江
-    *   @params:
-    *       barrier_two_b: 下面一块 (bottom)
-    *       barrier_two_t: 上面一块 (top)
-    *       sign_two: 关卡标志
-    * */
+     *   第二关: 强渡乌江
+     *   @params:
+     *       barrier_two_b: 下面一块 (bottom)
+     *       barrier_two_t: 上面一块 (top)
+     *       sign_two: 关卡标志
+     * */
     const barrier_three = new Circle({
         x: 60,
         y: winHeight - 1000,
@@ -739,11 +754,11 @@ $(document).ready(() => {
         img: document.querySelector("#img-title-3")
     });
     /*
-    *   第三关: 遵义会议
-    *   @params
-    *       barrier_three: 圆环 (中间一个缺口的)
-    *       sign_three: 关卡标志
-    * */
+     *   第三关: 遵义会议
+     *   @params
+     *       barrier_three: 圆环 (中间一个缺口的)
+     *       sign_three: 关卡标志
+     * */
     const barrier_four = new Circle({
         x: 60,
         y: winHeight - 1400,
@@ -787,11 +802,11 @@ $(document).ready(() => {
         img: document.querySelector("#img-title-5")
     });
     /*
-    *   第五关: 巧渡金沙江
-    *   @params:
-    *       barrier_five: 圆环 (中间三个缺口的)
-    *       sign_five: 关卡标志
-    * */
+     *   第五关: 巧渡金沙江
+     *   @params:
+     *       barrier_five: 圆环 (中间三个缺口的)
+     *       sign_five: 关卡标志
+     * */
     const barrier_six = new Circle({
         x: 60,
         y: winHeight - 2150,
@@ -811,11 +826,11 @@ $(document).ready(() => {
         img: document.querySelector("#img-title-6")
     });
     /*
-    *   第六关: 飞夺泸定桥 强渡大渡河
-    *   @params:
-    *       barrier_six: 圆环 (中间两个缺口的)
-    *       sign_six: 关卡标志
-    * */
+     *   第六关: 飞夺泸定桥 强渡大渡河
+     *   @params:
+     *       barrier_six: 圆环 (中间两个缺口的)
+     *       sign_six: 关卡标志
+     * */
     const barrier_seven_mountain = new Block({
         left: 220,
         top: winHeight - 2250,
@@ -848,12 +863,12 @@ $(document).ready(() => {
         img: document.querySelector("#img-title-7")
     });
     /*
-    *   第七关: 爬雪山 过草地
-    *   @params:
-    *       barrier_seven_mountain: 雪山
-    *       barrier_seven_grass: 草地
-    *       sign_seven: 关卡标志
-    * */
+     *   第七关: 爬雪山 过草地
+     *   @params:
+     *       barrier_seven_mountain: 雪山
+     *       barrier_seven_grass: 草地
+     *       sign_seven: 关卡标志
+     * */
     const barrier_eight = new Circle({
         x: 60,
         y: winHeight - 2650,
@@ -873,11 +888,11 @@ $(document).ready(() => {
         img: document.querySelector("#img-title-8")
     });
     /*
-    *   第八关: 突破腊子口
-    *   @params:
-    *       barrier_eight: 圆环 (中间一个缺口的)
-    *       sign_eight: 关卡标志
-    * */
+     *   第八关: 突破腊子口
+     *   @params:
+     *       barrier_eight: 圆环 (中间一个缺口的)
+     *       sign_eight: 关卡标志
+     * */
 
     stage.refresh();
 
@@ -894,35 +909,35 @@ $(document).ready(() => {
     }, 200);
 
     pub.touchTimer = window.setInterval(function () {
-         touch.blinkErase();
-         touch.blink();
+        touch.blinkErase();
+        touch.blink();
     }, 1000/60);
 
     /* 在 refresh 之后延时加载, 避免被擦掉, 只用画第一关, 其他的画了也看不到 */
 
-     $("#container").on("touchstart", function () {
+    $("#container").on("touchstart", function () {
 
-         if (pub.run === false) {
+        if (pub.run === false) {
 
-             window.clearInterval(pub.touchTimer);
-             /* 不让那小手那一块儿闪了, 跟着整个画布一起刷新 */
+            window.clearInterval(pub.touchTimer);
+            /* 不让那小手那一块儿闪了, 跟着整个画布一起刷新 */
 
-             controller.timer = window.setInterval(() => {
-                 gameTimer.run();
-                 $gameTimer.text(gameTimer.getTime());
-             }, 50);
-             /* 不是 canvas 部分的计时器 */
+            controller.timer = window.setInterval(() => {
+                gameTimer.run();
+                $gameTimer.text(gameTimer.getTime());
+            }, 50);
+            /* 不是 canvas 部分的计时器 */
 
-             controller.startTime = new Date();
-             /* 真正的游戏计时器 */
+            controller.startTime = new Date();
+            /* 真正的游戏计时器 */
 
-             $(document).on('touchstart', function () {
-                 star.jump();
-             });
+            $(document).on('touchstart', function () {
+                star.jump();
+            });
 
-             stage.run();
-             pub.run = true;
-             pub.isStart = true;
-         }
-     });
+            stage.run();
+            pub.run = true;
+            pub.isStart = true;
+        }
+    });
 });
